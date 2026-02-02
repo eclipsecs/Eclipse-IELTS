398292a884eee5f6fa52c4283303364ac775d8b3
Umerov Javokhir <1xedera@gmail.com>
2026-02-02 13:24:58 +0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -12,8 +12,8 @@ export enum QuestionType {
 }
 
 export type Theme = 'light' | 'dark';
-export type AppView = 'home' | 'test' | 'roadmap' | 'passage1' | 'passage2' | 'passage3';
-export type TestCategory = 'reading' | 'listening' | 'writing' | 'full';
+export type AppView = 'home' | 'test' | 'roadmap' | 'passage1' | 'passage2' | 'passage3' | 'article';
+export type TestCategory = 'reading' | 'listening' | 'writing' | 'full' | 'article';
 
 export interface Question {
   id: number;
